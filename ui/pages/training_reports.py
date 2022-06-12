fb604ca12ec6553008ae292fb79d9792862beaee
Archie <psibuck@gmail.com>
2022-06-12 08:26:38 +0100

    fix appearance of training reports page, update tasks

--- ui/pages/training_reports.py
+++ ui/pages/training_reports.py
@@ -20,15 +20,15 @@ class TrainingReports(PageBase):
 
     def setup_content(self):
         self.grid_columnconfigure(0, weight = 1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(1, weight = 10)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(1, weight = 1)
 
         self.venue_frame = Frame(self)
         self.venue_frame.grid(row = 0, column = 0, sticky = "nesw")
         
         self.training_list_frame = Frame(self)
-        self.training_list_frame.grid(row = 1, column = 0, sticky = "nesw")
+        self.training_list_frame.grid(row = 0, column = 1, sticky = "nesw")
 
         self.setup_venue_space()
         self.setup_training_space()
