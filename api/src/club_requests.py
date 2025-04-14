9296821af55558d5342db964e883954d1d161c82
psibuck <psibuck@gmail.com>
2025-04-14 08:51:40 +0100

    massive PITA - add a way to switch between website and app, simple solution but long time to get there

--- api/src/club_requests.py
+++
@@ -1,3 +0,0 @@
-
-
-def get_club_list()
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-
 import sys
 from src.application import Application
 from ui.page_manager import PageManager
+from fastapi import FastAPI
 
-is_debug = "debug" in sys.argv
+app = "app" in sys.argv
 
-# Refactor this, consider role of page manager vs app, same thing?
-app = Application(is_debug)
-ui = PageManager(app)
-ui.draw()
+if app:
+    is_debug = "debug" in sys.argv
+
+    # Refactor this, consider role of page manager vs app, same thing?
+    app = Application(is_debug)
+    ui = PageManager(app)
+    ui.draw()
+
+else:
+    app = FastAPI()
+    application = Application(True)
+    application.select_club(application.clubs[0])
+
+    @app.get("/")
+    def home():
+        return {"message": "Hello Radnor!"}
+
+    @app.get("/player_list")
+    def greet():
+        club_names = [club.name for club in application.clubs]
+        return {"message": f"{club_names}!"}
